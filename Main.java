7eb123fc1e3b450ace34c44e0964f854ac23cd7a
httpsbina <binafshab@nycstudents.net>
2024-05-31 13:50:12 +0000

    average sat of 900 is CRAZY.

--- Main.java
+++ Main.java
@@ -24,7 +24,31 @@ public class Main {
 
     // make sure all data entered the ArrayList
     System.out.println(totalScore);
-    System.out.println(totalScore.size());
+    System.out.println(totalScore.size() + " students took the exam.");
+    
+    // find highest score
+    int highestScore = totalScore.get(0);
+    for(int i = 1; i < totalScore.size(); i++){
+      if(totalScore.get(i) > highestScore)
+        highestScore = totalScore.get(i);
+    }
+    System.out.println("The highest score is: " + highestScore);
+
+    // find lowest score
+    int lowestScore = totalScore.get(0);
+    for(int i = 1; i < totalScore.size(); i++){
+      if(totalScore.get(i) < lowestScore)
+        lowestScore = totalScore.get(i);
+    }
+    System.out.println("The lowest score is: " + lowestScore);
+
+    // calculate the average score
+    int sum = 0;
+    for (int i = 0; i < totalScore.size(); i++) {
+      sum += totalScore.get(i);
+    }
+    double averageScore = (double) sum / totalScore.size();
+    System.out.println("The average score is: " + averageScore);
     
   }
 
